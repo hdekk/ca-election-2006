a611659c28fc39eb8b6d947fc7e5036520b63d73
Harrison Dekker <hdekker@uri.edu>
2019-02-01 10:28:15 -0500

    Add a regression analysis

--- scripts/plot.R
+++ scripts/plot.R
@@ -1,5 +1,4 @@
 #### Libraries
-library(foreign)
 library(ggplot2)
 
 # load data
@@ -22,3 +21,15 @@ plot1 <- ggplot(ca, aes(x=dem_pres_2004, y = prop_d)) + geom_point(size = 2) + t
        y = "% votes Dem candidate, 2006", 
        title = "Democratic vote share by county 2004, 2006")
 plot1
+
+# Regress the proportion of votes for the Democratic candidate, against the 
+# proportion of the two-party vote for the Democratic presidential candidate 
+# in 2004 in the district. Print the results and add the bivariate regression 
+# to the plot.
+
+bivariate <- lm(prop_d~dem_pres_2004, data = ca) 
+summary(bivariate)
+
+# adding the regression line to plot
+plot2 <- plot1 + geom_smooth(method = "lm", se = TRUE)
+plot2 
